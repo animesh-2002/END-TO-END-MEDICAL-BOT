00983a92c302222cc4abf160dec9198a42ad1b5f
animesh-2002 <93852453+animesh-2002@users.noreply.github.com>
2025-06-18 17:39:27 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+
+setup(
+
+
+    name="MEDICAL BOT GENERATIVE AI PROJECT",
+    version='0.0.0',
+    author='Animesh',
+    author_email='animeshkumarkar24@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
